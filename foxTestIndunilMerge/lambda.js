6a9a50f5fcdf007d542f9b9f7cac6066f1f40a4d
Indunil1992 <indunil@adroitlogic.com>
2019-09-03 17:02:21 +0530

    sns1

--- foxTestIndunilMerge/lambda.js
+++ foxTestIndunilMerge/lambda.js
@@ -1,4 +1,16 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed"});
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+
+exports.handler = function (event, context, callback) {
+    sns.listSubscriptionsByTopic({
+        TopicArn: 'arn:aws:sns:us-east-1:318300609668:testsnsindunil'
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
+
+    callback(null, { "message": "Successfully executed" });
 }
