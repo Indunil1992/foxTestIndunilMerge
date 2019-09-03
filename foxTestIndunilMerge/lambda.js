391f6505169c93e0049e1e4bef27eb9684697783
Indunil1992 <indunil@adroitlogic.com>
2019-09-03 17:12:31 +0530

    sns2

--- foxTestIndunilMerge/lambda.js
+++ foxTestIndunilMerge/lambda.js
@@ -6,10 +6,16 @@ exports.handler = function (event, context, callback) {
         TopicArn: 'arn:aws:sns:us-east-1:318300609668:testsnsindunil'
     }).promise()
         .then(data => {
+                        console.log(data);
+                                    console.log("successs");
+
+
             // your code goes here
         })
         .catch(err => {
             // error handling goes here
+            console.log(err);
+            console.log("errorrr");
         });
 
     callback(null, { "message": "Successfully executed" });
