0c90fd4837c8e1e40bcf9395a0a3d5ad73da97eb
Indunil1992 <indunil@adroitlogic.com>
2019-09-04 10:32:24 +0530

    removeSES

--- foxTestIndunilMerge/lambda.js
+++ foxTestIndunilMerge/lambda.js
@@ -1,30 +1,4 @@
-let AWS = require('aws-sdk');
-const ses = new AWS.SES();
-
 exports.handler = function (event, context, callback) {
-    ses.sendEmail({
-        Destination: {
-            ToAddresses: ['indunil@adroitlogic.com'],
-            CcAddresses: ['sachithrarajapakse1992@gmail.com'],
-            BccAddresses: []
-        },
-        Message: {
-            Body: {
-                Text: {
-                    Data: `test Text Body`
-                }
-            },
-            Subject: {
-                Data: 'test subject'
-            }
-        },
-        Source: 'indunil@adroitlogic.com',
-    }, function (err, data) {
-        if (err) {console.log(err, err.stack); // an error occurred
-        console.log("an error occurred:SES");}
-        else console.log(data);           // successful response
-        console.log("successful response:SES");
-    });
 
     callback(null, { "message": "Successfully executed" });
 }
