7fb87a65937a71b78a146f8fa7560b92e67e57ec
Indunil1992 <indunil@adroitlogic.com>
2019-09-04 12:12:12 +0530

    add API and SES

--- foxTestIndunilMerge/lambda.js
+++ foxTestIndunilMerge/lambda.js
@@ -1,5 +1,29 @@
+let AWS = require('aws-sdk');
+const ses = new AWS.SES();
+
 exports.handler = function (event, context, callback) {
-    debugger;
+
+    ses.sendEmail({
+        Destination: {
+            ToAddresses: ['indunil@adroitlogic.com'],
+            CcAddresses: ['indunil@adroitlogic.com'],
+            BccAddresses: []
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: `Body body `
+                }
+            },
+            Subject: {
+                Data: 'test Merge '
+            }
+        },
+        Source: 'indunil@adroitlogic.com',
+    }, function (err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data);           // successful response
+    });
 
     callback(null, { "message": "Successfully executed" });
 }
